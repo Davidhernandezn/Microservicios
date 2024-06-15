e3892d4d13ed126b9c330a88d93dc067be2f66e5
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-06-15 17:23:56 -0600

    Configuracion del EntityScan

--- microservicios-respuestas/src/main/java/com/microservicios/respuestas/MicroserviciosRespuestasApplication.java
+++ microservicios-respuestas/src/main/java/com/microservicios/respuestas/MicroserviciosRespuestasApplication.java
@@ -2,9 +2,18 @@ package com.microservicios.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 //HABILITAR
-@EnableEurekaClient //OPCIONAR YA QUE CON EL POM ESTA HABILITADO@SpringBootApplication
+@EnableEurekaClient //OPCIONAR YA QUE CON EL POM ESTA HABILITADO
+@SpringBootApplication
+
+//Configurarlo para reconocer los commons alumnos y examenes
+@EntityScan({"com.microservicios.respuestas.models.entity",
+			 "com.microservicios.commons.alumnos.models.entity",
+			 "com.commons.examenes.models.entity"})
+
 public class MicroserviciosRespuestasApplication {
 
 	public static void main(String[] args) {
