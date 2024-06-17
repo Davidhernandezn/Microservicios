258ce6cd15cf415fd0b6527f880364656539b2dc
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-06-16 23:20:53 -0600

    Consulta JPQL con join para las respuestas del alumno por examen

--- microservicios-respuestas/src/main/java/com/microservicios/respuestas/models/repository/RespuestaRepository.java
+++ microservicios-respuestas/src/main/java/com/microservicios/respuestas/models/repository/RespuestaRepository.java
@@ -1,9 +1,16 @@
 package com.microservicios.respuestas.models.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.microservicios.respuestas.models.entity.Respuesta;
 
 public interface RespuestaRepository extends CrudRepository<Respuesta, Long> {
-
+	
+	//RETORNAR LIST O ITERABLE DE RESPUESTA
+	/*R ES EL OBJETO DE LA CLASE ENTITY ALIAS FETCH (PARA RETORNAR LA RESPUESTA CO EL ALUMNO Y EL EXAMEN)JOIN  R.ALUMNO
+	 * INNER JOIN COMO EN BD COMPARAMOS CON REFERENCIA A LOS ATRIBUTOS DE LA RELACION */
+	@Query("select r from Respuesta r join fetch r.alumno a join fetch r.pregunta p join fetch p.examen e where a.id=?1 and e.id=?2")
+	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId);
+	
 }
