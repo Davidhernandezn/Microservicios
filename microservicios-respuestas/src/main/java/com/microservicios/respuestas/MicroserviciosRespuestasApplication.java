e108f61aff7712f5e0d276a90ea5d9636733a4df
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-06-18 01:06:00 -0600

    Prueba levantamiento de microservicios

--- microservicios-respuestas/src/main/java/com/microservicios/respuestas/MicroserviciosRespuestasApplication.java
+++ microservicios-respuestas/src/main/java/com/microservicios/respuestas/MicroserviciosRespuestasApplication.java
@@ -1,11 +1,17 @@
 package com.microservicios.respuestas;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 //HABILITAR
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @EnableEurekaClient //OPCIONAR YA QUE CON EL POM ESTA HABILITADO
 @SpringBootApplication
 
